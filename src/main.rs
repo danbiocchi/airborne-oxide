7c8a3a5a5a86edc28ef97c05971dda5544353281
Daniel <danielbiocchi@hotmail.com>
2024-07-24 18:09:03 -0400

    Blinker test program for Matek VTOL405 flight controller

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+use cortex_m_rt::entry;
+use stm32f4xx_hal::{pac, prelude::*};
+
+#[entry]
+fn main() -> ! {
+    let dp = pac::Peripherals::take().unwrap();
+    let rcc = dp.RCC.constrain();
+    let _clocks = rcc.cfgr.sysclk(48.MHz()).freeze();
+
+    let gp = dp.GPIOA.split();
+    let mut led = gp.pa3.into_push_pull_output();
+
+    loop {
+        led.set_high();
+        cortex_m::asm::delay(8_000_000);
+        led.set_low();
+        cortex_m::asm::delay(8_000_000);
+    }
 }
